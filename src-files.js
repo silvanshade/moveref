16949f256422005f4eb160373b79d07754cf3408
silvanshade <silvanshade@users.noreply.github.com>
2023-08-15 19:55:11 +0000

    deploy: 9f57ee742889b3efc1435b955fbb85c167b3c2a5

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "moveref":["",[],["deref_move.rs","emplace.rs","into_move.rs","lib.rs","macros.rs","move_ref.rs","new.rs","slot.rs","slot_storage.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();

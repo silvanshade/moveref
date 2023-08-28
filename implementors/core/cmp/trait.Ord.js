511ffea8336f4a934659639e1aea60f356100e31
silvanshade <silvanshade@users.noreply.github.com>
2023-08-28 15:33:18 +0000

    deploy: 1a73b2442e9b183d5702ded80d3cdbed47ecad6b

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"moveref":[["impl&lt;'t, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"moveref/struct.MoveRef.html\" title=\"struct moveref::MoveRef\">MoveRef</a>&lt;'t, T&gt;<span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>,</span>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
